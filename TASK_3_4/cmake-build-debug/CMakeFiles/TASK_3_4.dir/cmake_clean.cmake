73d73f7db1389ba23d7b57957bfe5fee5b89b860
greraldicus <gbeshlyaga@gmail.com>
2021-11-29 00:25:10 +0500

    3_4

---
+++ TASK_3_4/cmake-build-debug/CMakeFiles/TASK_3_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TASK_3_4.dir/main.cpp.obj"
+  "TASK_3_4.exe"
+  "TASK_3_4.exe.manifest"
+  "TASK_3_4.pdb"
+  "libTASK_3_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TASK_3_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
