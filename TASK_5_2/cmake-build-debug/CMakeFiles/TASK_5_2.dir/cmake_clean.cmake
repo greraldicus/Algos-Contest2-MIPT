ebffadaa8cf1cfec34afdf86c7e1415b2c734707
greraldicus <gbeshlyaga@gmail.com>
2021-11-29 18:06:40 +0500

    5_2

---
+++ TASK_5_2/cmake-build-debug/CMakeFiles/TASK_5_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TASK_5_2.dir/main.cpp.obj"
+  "TASK_5_2.exe"
+  "TASK_5_2.exe.manifest"
+  "TASK_5_2.pdb"
+  "libTASK_5_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TASK_5_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
